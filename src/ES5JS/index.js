93989e9f772a7e206f69babc33a39e32fdb69bfb
Jzz8977 <jzz8977@163.com>
2019-03-22 23:27:16 +0800

    主页已经完成，只差完美轮播

--- src/ES5JS/index.js
+++ src/ES5JS/index.js
@@ -10,47 +10,56 @@ $(function () {
     $('.game-table-hidden').css('display', 'none');
     return false;
   });
-  $('.banner-left li:not(.banner-left-firstli)').mouseenter(function () {
+  $('.banner-left>li:not(.banner-left-firstli)').mouseenter(function () {
     $(this).css({
       background: '#fff',
       color: '#61656e'
-    });
+    }).children('div').css('display', 'block');
   });
-  $('.banner-left li').mouseleave(function () {
+  $('.banner-left>li').mouseleave(function () {
     $(this).css({
       background: '#ee4545',
       color: '#fff'
+    }).children('div').css('display', 'none');
+  });
+  $('#game .game-section-middle li').not(".game-section-middle-minh").mouseenter(function () {
+    $(this).animate({
+      paddingLeft: 25
+    }, 100);
+  });
+  $('#game .game-section-middle li').not(".game-section-middle-minh").mouseleave(function () {
+    $(this).finish();
+    $(this).animate({
+      paddingLeft: 15
+    }, 100);
+  });
+});
+$(function () {
+  var $ul = $('.banner-middle ul');
+  var $ol = $('.banner-middle ol');
+  var $ulli = $('.banner-middle ul li');
+  var $olli = $('.banner-middle ol li');
+  var timerA = null;
+  var index = 4;
+  $ulli.each(function () {
+    $(this).find('p').html($(this).find('img').attr('alt'));
+  });
+  setInterval(function () {
+    console.log(index + "a"); // console.log(index);
+
+    $ulli.eq(index--).stop().animate({
+      left: -770
+    }, 500, function () {
+      $(this).prependTo($ul);
+      $(this).animate({
+        left: 0
+      }, 200, function () {});
     });
-  }); // var $ul = $('.banner-middle ul');
-  // var $ol = $('.banner-middle ol');
-  // var $uli = $('.banner-middle ul li');
-  // var $oli = $('.banner-middle ol li');
-  // var timerA = null;
-  // var index =0;
-  // $uli.each(function(){
-  // 	$(this).find('p').html($(this).find('img').attr('alt'));
-  // })
-  // var timer1 =setInterval(autoPlay,1000);
-  // 	timerA = setInterval(function(){
-  // 		index ++;
-  // 		if(index ==$uli.size()){
-  // 			index=0;
-  // 		}
-  // 	},5000);
-  // 	Go();
-  // 	function Go()	{
-  // 		$ul.css({left : -770});
-  // 		$oli.eq(index).addClass('red').siblings().removeClass('red');
-  // 	}
-  // 	$uli.mouseenter(function(){
-  // 		clearInterval(timerA);
-  // 			$oli.click(function(){
-  // 	//		alert($(this).index() +1);
-  // 			index = $(this).index() +1;
-  // 			Go();
-  // 		})
-  // 	})
-  // 	$uli.mouseleave(function(){
-  // 		Go();
-  // 	})
+
+    if (index == -1) {
+      index = 4;
+    } // $ul.animate({"left": "60%"},500);
+    // $ul.animate({"left": "50%"},500);
+
+  }, 2000);
 });
